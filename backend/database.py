93ef06163d0e9854b3e67af4a3d74b9a8b7c446f
JayadipReddy <jayadip.reddy@eminds.ai>
2026-02-07 16:01:16 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@trekky-hub-db:5432/trekky-hub"
+DATABASE_URL = "postgresql://postgres:root@trekky-hub-db:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 
