fa48506de1b66f6cc0d35fb7f797c15bc77cf9b3
Navata Poojari <navathapoojari123@gmail.com>
2026-02-10 15:13:57 +0530

    Navatha code.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@trekky-hub-db:5432/postgres"
+DATABASE_URL = "postgresql://postgres:root@localhost:5432/trekky-hub-db"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
