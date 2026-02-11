07a3781cd921497884dcf14a8f1a266a994d77ef
JayadipReddy <jayadip.reddy@eminds.ai>
2026-02-11 17:11:03 +0530

    Update page.tsx

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -21,7 +21,7 @@ export default function RegisterPage() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://backend-service:8000/register", {
+      const response = await fetch("api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
